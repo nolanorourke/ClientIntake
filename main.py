d2dd3a717717a72fea8c3f7aa384cc23745805c6
Nolan ORourke <ngorourke@gmail.com>
2025-04-07 17:29:40 -0500

    made some changes to python file, got it started

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import sys
+import tkinter as tk
+from tkinter import messagebox
+import datetime
 
+def submit_data(self, name, reason, date_ocurred, location_ocurred):
+    print(f"Name: {name}")
+    print(f"Reason: {reason}")
+    print(f"Date Occurred: {date_ocurred}")
+    print(f"Location: {location_ocurred}")
+
+
+if __name__ == "__main__":
+    
