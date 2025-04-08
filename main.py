3197d67d561ec515d4102c31c126eda123d0cd9b
Nolan ORourke <ngorourke@gmail.com>
2025-04-08 15:35:36 -0500

    functional python app so far, have yet to make it store anything but works correctly and looks good enough

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def display_data(name, reason, date_ocurred, location_ocurred):
 def generate_UI():
     window = tk.Tk()
     window.title("Client Intake Form")    
+    window_width = int(window.winfo_screenwidth() * .6)
+    window_height = int(window.winfo_screenheight() * .6)
+    window.geometry(f"{window_width}x{window_height}")
 
     name_label = tk.Label(window, text="Name:")
     name_label.grid(row=0, column=0)
@@ -33,6 +36,32 @@ def generate_UI():
     location_entry = tk.Entry(window)
     location_entry.grid(row=3, column=1)
 
+    def submit():
+        name = name_entry.get().strip()
+        reason = reason_entry.get().strip()
+        date = date_occurred_entry.get().strip()
+        location = location_entry.get().strip()
+
+        if not name:
+            name = "NO NAME"
+        if not reason:
+            reason = "NO REASON"
+        if not date:
+            date = "NO DATE"
+        if not location:
+            location = "NO LOCATION"
+
+        display_data(name, reason, date, location)
+
+        name_entry.delete(0, tk.END)
+        reason_entry.delete(0, tk.END)
+        date_occurred_entry.delete(0, tk.END)
+        location_entry.delete(0, tk.END)
+
+        messagebox.showinfo("Success!", "Data submitted successfully")
+    
+    submit_button = tk.Button(window, text="Submit", command=submit)
+    submit_button.grid(row=4, column=0, columnspan=2)
 
     window.mainloop()
 
